881b549ca658644381a9905ef0d2b77e6d135139
ArnaudM22 <82112143+ArnaudM22@users.noreply.github.com>
2021-11-24 20:01:35 +0100

    Add files via upload

---
+++ PHPpagesdir/pagesphp/testArnaud/adminmodif.php
@@ -0,0 +1,13 @@
+<?php require_once 'db_utils.php';
+connect_db();
+    if(isset($_POST['submit']))
+    {
+    #pour toutes les lignes du tableau
+        #si checkbox cochée
+            #requete de supression de la ligne dans la base de donnée avec clé
+        #si rôle différent de role bd
+            #rôle prend nouvelle valeur
+        #astuce : creer array php, créer table html qui s'affiche avec css
+disconnect_db();
+?>
+
--- PHPpagesdir/pagesphp/testArnaud/adminpage.php
+++ PHPpagesdir/pagesphp/testArnaud/adminpage.php
@@ -66,58 +66,51 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <div class="table-container">
-        <h1 class="heading">Genome for annotation </h1>
-        <?php
-        //connexion to mysql database
-        $servername ="localhost:3306";
-        $username = "root";
-        $password="root";
-        $dbname="projetweb";
+        <h1 class="heading">User list </h1>
+        <?php require_once 'db_utils.php';
+        connect_db();
 
-        //try, catch vérifie que la connexion à mysql est établie
-        try{
-            $conn = new PDO("mysql: host=$servername; dbname=$dbname", $username, $password);
-        }
-        catch(PDOException $e){
-            echo "la connexion a échoué:" . $e-> getMessage();
-        }
+        $result = pg_query($db_conn,"SELECT * FROM w_gene.users");
+        echo "<form action='adminmodif.php' method='post'>";
+            echo "<table class='table'>
+                    <thead>
+                        <tr>
+                       <th> First Name </th>
+                        <th> Family Name </th>
+                        <th> Role </th>
+                        <th> Email </th>
+                        <th> Phone Number  </th>
+                        <th> Date </th>
+                        <th> Delete </th>
+                        <th> Affect role</th>
+                        </tr>
+                    </thead>";
 
-        $result = mysqli_query($conn,"SELECT * FROM users");
-
-        echo "<table border='1'>
-            <tr>
-           <th> First Name </th>
-            <th> Family Name </th>
-            <th> Role </th>
-            <th> Email </th>
-            <th> Phone Number  </th>
-            <th> Date </th>
-            <th> Hour </th>
-            <th> Delete </th>
-            <th> Affect role</th>
-
-             </tr>";
-
-        while($row = mysqli_fetch_array($result))
-        {
-            echo "<tr>";
-            echo "<td>" . $row['surname'] . "</td>";
-            echo "<td>" . $row['name'] . "</td>";
-            echo "<td>" . $row['role'] . "</td>";
-            echo "<td>" . $row['email'] . "</td>";
-            echo "<td>" . $row['phone'] . "</td>";
-            echo "<td>" . $row['date'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-
-        mysqli_close($conn);
+                while($row = pg_fetch_array($result))
+                {
+                    echo "<tr>";
+                    echo "<td>" . $row['surname'] . "</td>";
+                    echo "<td>" . $row['name'] . "</td>";
+                    echo "<td>" . $row['role'] . "</td>";
+                    echo "<td>" . $row['email'] . "</td>";
+                    echo "<td>" . $row['phone'] . "</td>";
+                    echo "<td>" . $row['date'] . "</td>";
+                    echo"<td>","<input type='checkbox' name= 'delete'>","</td>";
+                    echo"<td>","<select name='role'>",
+                        "<option value= 'reader'>Reader</option>",
+                        "<option value= 'annotator'>Annotator</option>",
+                        "<option value= 'validator'>Validator</option>",
+                        "</select>","</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            echo "<input type='submit' name = 'submit' value='Soumettre'>";
+        echo"</form>";
+        disconnect_db();
         ?>
     </div>
-                    <tbody>
-                </table><br> 
-                <input type="button" value="Soumettre">
-           
-           <a href="ind.php" class ="button">logout</a>
+
+    <a href="ind.php" class ="button">logout</a>
 
         </div>
+</html>
