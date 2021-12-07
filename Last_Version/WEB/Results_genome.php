b42c59255b23cf1b1a35ad81d14b9502e63e91b6
ArnaudM22 <82112143+ArnaudM22@users.noreply.github.com>
2021-12-07 21:31:38 +0100

    Add files via upload

--- Last_Version/WEB/Results_genome.php
+++ Last_Version/WEB/Results_genome.php
@@ -1,84 +1,113 @@
 <!DOCTYPE html>
 <?php require_once 'db_utils.php';
+# connexion à la base : affiche "connection failed" si pas de connection
 connect_db();
 ?>
 <html>
-  <head>
-  <meta charset="utf-8" />
-        <title> Results pages</title>
-	 <link rel="stylesheet" type="text/css" href="reader.css">
-  </head>
 
- <body>
- <!-- Menu de navigation -->
- <nav>
-     <div class="nav-content">
-         <div class="logo">
-             <a href="#">GenAnnot.</a>
-         </div>
-         <ul class="nav-links">
-             <li><a href="Home_page.php">Home</a></li>
-             <li><a href="#">Admin</a></li>
-             <li><a href="#">Validator</a></li>
-             <li><a href="#">Annotator</a></li>
-             <li><a href="#">Reader</a>
-                 <ul class="sous-menu">
-                     <li class = "sous-menu1"><a href="#">Form</a></li>
-                     <ul class="sous-sous-menu">
-                         <li class="sous-menu2"><a href="Form_genome.php">Genomes Form</a></li>
-                         <li class="sous-menu2"><a href="Form_cds.php">Genes/Prot Form</a></li>
-                         <!--TODO: sous menu apparait quand tu passes ta souris-->
-                     </ul>
-                 </ul>
-             </li>
-             <li><a href="#">Logout</a>
-                 <!--signIn.php-->
-         </ul>
-     </div>
- </nav>
+<head>
 
- <div class ="container">
-     <div class="title"> Genome Results </div><br>
-        <table>
-    	<?php
-        $str=$_SERVER['REQUEST_URI'];
-	    $keywords = preg_split("/=/", $str);
-	    $accessionnb= $keywords[1];
+	<meta charset="utf-8" />
+	<title> Results pages</title>
+	<link rel="stylesheet" type="text/css" href="signin.css">
+</head>
 
-        $result = pg_query($db_conn,"SELECT * FROM w_gene.genome WHERE accessionnb='".$accessionnb."';");
-	    if (!$result) {
- 		    echo "Une erreur s'est produite.\n";
-  	    exit;
-	    }
+<body>
 
-        while ($row = pg_fetch_assoc($result)){
-	        echo "<tr><th> Accession Number : </th><td> ".$row['accessionnb']."</td></tr>
-	              <tr><th> Species : </th><td> ".$row['species']."</td></tr>
-	              <tr><th> Strain : </th><td> ".$row['strain']."</td></tr>
-	              <tr><th> Sequence length : </th><td> ".$row['seq_length']."</td></tr>";
-                  $seq_nt = $row['seq_nt'];
-	    }
-        ?>
-        </table>
+	<!-- Fiche Genome -->
+	<div class='resultat'>
+		<h2>Fiche</h2>
+		<table style='width:10%' ;>
 
-        <br><br>
+			<?php
+			$a = 0;
+			$b = 5000;
+			$str = $_SERVER['REQUEST_URI'];
+			$keywords = preg_split("/=/", $str);
+			$accessionnb = $keywords[1]; // id recupéré de l'url
+			?>
+			<?php
 
-        <strong>Genome Sequence : </strong>
-        <textarea name="text" cols="55" rows="20" id="text">
-            <?php echo $seq_nt; ?>
-        </textarea>
+			#stocakge du genome
+			$res1 = pg_query($db_conn, "SELECT * FROM w_gene.genome WHERE accessionnb='ASM666v1';");
+			if (!$res1) {
+				echo "Une erreur s'est produite.\n";
+				exit;
+			}
+			echo " <td colspan='2'> Informations générales </td>";
+			while ($row1 = pg_fetch_assoc($res1)) {
+				echo "<div style='font-size:110%'> 
+	<tr><th> Accession Number </th><td>" . $row1['accessionnb'] . "</td></tr><br>
+	<tr><th> Species </th><td>" . $row1['species'] . "</td></tr><br>
+	<tr><th> Strain </th><td>" . $row1['strain'] . "</td></tr><br>
+	<tr><th> Sequence length </th><td>" . $row1['seq_length'] . "</td></tr><br>
+        </div>";
+				$genome = $row1['seq_nt'];
+			}
 
-     <!--TODO : fonctionnalités télécharger les résultats + css bouton qui n'est pas un form-->
-     <div class="button">
-         <input type="submit" name="ddl_results" value="Download results"><br>
-     </div>
+			#stockage variables sequence
+			$query = "SELECT * FROM w_gene.sequence WHERE accessionnb='ASM666v1' AND cds_start > ".$a." AND cds_start < ".($b).";";
+			print_r($query);
+			$res2 = pg_query($db_conn, $query );
+			if (!$res2) {
+				echo "Une erreur s'est produite.\n";
+				exit;
+			}
+			$res2table = pg_fetch_all($res2);
+			array_multisort($debutsequence = array_column($res2table, 'cds_start'), SORT_ASC, $res2table);
+			$sequence = array_column($res2table, 'cds_seq');
+			$debutsequence = array_column($res2table, 'cds_start');
+			$finsequence = array_column($res2table, 'cds_end');
+			$longsequence = array_column($res2table, 'cds_size');
+			$idsequence = array_column($res2table, 'idsequence');
 
-</div>
- </body>
+			?>
+		</table>
+		<h4>Séquence : </h4>
+		<div class="card-container">
+			<div class="card">
+				<div class="genome">
+					<?php
+					$partial_genome = substr($genome, $a, $b);
+					echo $partial_genome;
+					?>
+
+					<?php
+					$char_width = 10;
+					$space = 34;
+					for ($k = $a; $k <= $b; $k = $k + 10) {
+						$style = 'style="left:' . $k . 'ch "';
+						echo '<div class="labely"  ' . $style . '><div class="labeltext">';
+						echo $k;
+						echo '</div></div>';
+						echo '<div class="label-bar" ' . $style . '></div>';
+						// echo "prout";
+					}
+					$Niv = array_fill(0, 99, 0);
+					$seqpage = array_slice($debutsequence, $a, $b-$a);
+					// print_r($seqpage);
+					
+					for ($i = 0; $i < count($seqpage); $i++) {
+						$j = 0;
+						while($seqpage[$i] < $Niv[$j] ) :
+							// TODO incrémenter .append niv si $j > len($niv)
+							$j++;
+						endwhile;
+						$Niv[$j] = $finsequence[$i];
+						echo '<input type="button" value="'.$idsequence[$i].'" class="bouton-seq-container" style="top: '.(($j)*$space+20).'px; left: '.$seqpage[$i].'ch; width: '.$longsequence[$i].'ch">';
+						// echo "<input type='button' class='button-seq' value='".$idsequence[$i]."' />";
+						echo '</input>';
+					};
+					?>
+
+
+				</div>
+				<!--Afficher sequence gene en couleu, en cliquant sur un gène, redirige vers la page gène associée -->
+			</div>
+		</div>
+	</div>
+	</textarea>
+
+	</div>
+
 </html>
-
-<?php
-disconnect_db();
-?>
-
-
