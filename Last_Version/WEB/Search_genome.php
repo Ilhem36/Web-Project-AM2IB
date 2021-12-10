e6db19d2618e16cb9d4f966618a8abb44c5cd2a6
Lorraine-Wg <70911054+Lorraine-Wg@users.noreply.github.com>
2021-12-10 08:22:41 +0100

    comments

--- Last_Version/WEB/Search_genome.php
+++ Last_Version/WEB/Search_genome.php
@@ -1,6 +1,7 @@
 <?php
+//This is the page where we prepare the sql queries and display the results in a table, the accession number is cliquable and leads to Result_genome.php page
 require_once 'db_utils.php';
-connect_db();
+connect_db();//connexion to the database
 session_start(); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -32,15 +33,17 @@ session_start(); ?>
 
           <!-- Genomes Results List-->
           <?php
-	        $query_sql = "";
+	        $query_sql = "";//initialisation
+	  //the values from the form Form_genome.php
 	        $field_form = ["accessionnb","species","strain","seq_length", "seq_nt"];
+	   //the attributs from the sql table genome 
 	        $db_gc_col = ["accessionnb","species","strain","seq_length","seq_nt"];
 	        for ($i = 0; $i <= 4; $i++) {
 		        $ff =  $field_form[$i];
 		        $gc = $db_gc_col[$i];
-                if (!empty($_GET[$ff])){
+                if (!empty($_GET[$ff])){//if the fields from the form are not empty
 			        if (strlen($query_sql) > 5){
-				        if($ff != "seq_nt"){
+				        if($ff != "seq_nt"){//if we don't request for genome sequence 
 					    $query_sql .= "AND ".$gc."='".$_GET[$ff]."'";
 				        }else{
 					    $query_sql .= "AND ".$gc." LIKE '%".$_GET[$ff]."%' ";
@@ -56,10 +59,10 @@ session_start(); ?>
 		        }
 	        }
 
-	        if(strlen($query_sql) > 5){
-		        $query_sql .= ";";
-		        $result = pg_query($db_conn,$query_sql);
-                if (!$result) {
+	        if(strlen($query_sql) > 5){ 
+		        $query_sql .= ";"; //put a comma to the end of the sql query
+		        $result = pg_query($db_conn,$query_sql); //the sql query is prepared and stocked in "result" variable 
+                if (!$result) {//No results
                     echo "An error has occurred. Please, retry.\n";
                 exit;
                 }
@@ -68,7 +71,7 @@ session_start(); ?>
                     echo "There is no result for your request. <br>";
                 }
 
-                else if(pg_num_rows($result) != 0) { //Results
+                else if(pg_num_rows($result) != 0) { //Display Results in a table
                     echo "<table class='table'>
                           <thead>
                           <tr>
@@ -80,7 +83,7 @@ session_start(); ?>
                           </thead>";
                     while ($row = pg_fetch_assoc($result) ){
                         echo "<tr>
-                                   <td><a href='Results_genome2.php?id=".$row['accessionnb']."'>".$row['accessionnb']."</a></td>  
+                                   <td><a href='Results_genome2.php?id=".$row['accessionnb']."'>".$row['accessionnb']."</a></td>  //the accession number is clickable
                                    <td>".$row['species']."</td>
 	    	                       <td>".$row['strain']."</td>
                                    <td>".$row['seq_length']."</td>
@@ -89,7 +92,7 @@ session_start(); ?>
                     }
                     echo "</table>";
             }
-            disconnect_db();
+            disconnect_db(); //deconnexion from the database
             ?>
 </body>
 </html>
